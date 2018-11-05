40c5d677dacf25e85c877638d507dcc98aca99f3
iamilyasedunov <iliasedunov@yandex.ru>
2018-11-05 19:24:12 +0300

    the correct reference to the array added in task3.h

--- include/task3.h
+++ include/task3.h
@@ -2,7 +2,7 @@
 using namespace std;
 
 template <typename T,  size_t n>
-void map(T arr[],T (* change)(T)){
+void map(T (&arr)[n],T (* change)(T)){
     for(size_t i = 0; i < n; i++){
         arr[i] = change(arr[i]);
     }
