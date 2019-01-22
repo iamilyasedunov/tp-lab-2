295187be6b7737be8a8c64e8b245ce84a6e353a0
iamilyasedunov <36935089+iamilyasedunov@users.noreply.github.com>
2019-01-22 18:54:09 +0300

    Update task3.h

--- include/task3.h
+++ include/task3.h
@@ -2,7 +2,7 @@
 using namespace std;
 
 template <typename T,  size_t n>
-void map(T (&arr)[n],T (* change)(T)){
+void map(T (&arr)[n],T (* change)(T)){  //link on the mas which consists from n elements and pointer on the function with parameter 
     for(size_t i = 0; i < n; i++){
         arr[i] = change(arr[i]);
     }
