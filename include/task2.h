67db0eae59fe630634518f8518fb9fa701aa03f7
iamilyasedunov <36935089+iamilyasedunov@users.noreply.github.com>
2019-01-22 18:48:48 +0300

    Update task2.h
    comment for pointer

--- include/task2.h
+++ include/task2.h
@@ -5,7 +5,7 @@
 using namespace std;
 
 template <typename T, size_t n>
-T* createArr(T (*gen)()){
+T* createArr(T (*gen)()){       //pointer on the function
     T* mas = new T[n+1];
     for (size_t i = 0; i < n; i++) {
         mas[i] = gen();
